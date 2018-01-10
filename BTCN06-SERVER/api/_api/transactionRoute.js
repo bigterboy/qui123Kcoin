00b8f640ff03fa89c6f7cd0a91023d50378df0fd
nhockungfu <vsp1308@gmail.com>
2018-01-10 13:51:13 +0700

    transaction route

--- BTCN06-SERVER/api/_api/transactionRoute.js
+++ BTCN06-SERVER/api/_api/transactionRoute.js
@@ -1,4 +1,15 @@
 var express = require('express');
 var r = express.Router();
+var userController = require('../controllers/userController');
+
+r.post('/transaction', userController.login);
+r.post('/transaction-verify', userController.check_verify);
+r.post('/sign-up', userController.sign_up);
+
+r.get('/transaction-user-info', userController.get_info_user);
+r.get('/transaction/test', userController.authenticate);
+r.get('/transaction-info', userController.clear_user_info);
+
 
 module.exports = r;
+
